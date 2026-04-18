b010364e9138f8d9a45f760a4859402b2673491a
hamzasand <mhamzasaeed762@gmail.com>
2026-04-18 12:29:05 +0500

    create and save .pkl file

--- Mlops/models_code/regression.py
+++ Mlops/models_code/regression.py
@@ -1,9 +1,10 @@
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
 from sklearn.compose import ColumnTransformer
 from sklearn.linear_model import SGDRegressor
+import joblib
 
 class LinearRegressionModel:
     def __init__(self):
@@ -37,6 +38,7 @@ class LinearRegressionModel:
 
         X_train = preprocessor.fit_transform(X_train)
         X_test = preprocessor.transform(X_test)
+        return X_test,X_train
 
     def train_model(self, X_train, y_train):
         sgd_model = SGDRegressor(loss="squared_error", max_iter=1000, learning_rate="invscalling", eta=0.01)
@@ -48,6 +50,8 @@ if __name__ == "__main__":
     data = model.load_data("C:\Users\92324\Desktop\ML_from_Sratch\ML_from_Scratch\Mlops\data\housing_price_dataset.csv")
     data = model.drop_feature(data, "ID")
     X, y = model.create_featur_label(data, "Price")
-    X_train, X_test, y_train, y_test =model.train_test_split()
+    X_train, X_test, y_train, y_test =model.train_test_split(X,y)
     X_train = model.standarized_data(X_train, X_test)
     sgd_model = model.train_model(X_train, y_train)
+
+    joblib.dump(sgd_model, "linear_regression_model.pkl")
