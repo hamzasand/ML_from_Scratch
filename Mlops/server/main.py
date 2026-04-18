3023d951af3dab5660b79c1dbea2f7ff651fea2f
hamzasand <mhamzasaeed762@gmail.com>
2026-04-18 15:43:59 +0500

    genrate the main.py in fastapi

---
+++ Mlops/server/main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI, Request
+import uvicorn
+from fastapi.templating import Jinja2Templates
+from controllers.controller import router
+
+app = FastAPI()
+app.include_router(router)
+app.mount('/static',StaticFiles(directory='static'), name='static')
+
+templates = Jinja2Templates(directory='templates')
+
+@app.get("/")
+async def homepage(request:Request):
+    return templates.TemplateResponse("index.html",{"request":request})
+
+if __name__ == "__main__":
+    uvicorn.run(app,host="0.0.0.0",port=5000)
