72af5919e4f6670b09d3b3a6523a7b1ec7f8cd8f
hamzasand <mhamzasaeed762@gmail.com>
2026-04-18 12:58:29 +0500

    Generate the pkl file ad added

--- Mlops/models_code/regression.py
+++ Mlops/models_code/regression.py
@@ -15,43 +15,61 @@ class LinearRegressionModel:
         return data
     
     def drop_feature(self, data, feature_name):
-        data = data.drop(column=[feature_name])
+        data = data.drop(columns=[feature_name])
+        return data
     
     def create_featur_label(self, data, feature_name):
-        X = data.drop(columns=feature_name)
+        X = data.drop(columns=[feature_name])
         y = data[feature_name]
         return X, y
     
     def train_test_split(self, X, y, test_size=0.2, random_state=42):
         return train_test_split(X,y, test_size=0.2, random_state=42)
     
-    def standarized_data(self, X_train, X_test):
-        numeric_features = ["SquareFeet","BedRooms", "Bathrooms"]
+    # def standarized_data(self, X_train, X_test):
+    #     numeric_features = ["SquareFeet","Bedrooms", "Bathrooms"]
+    #     categorical_features = ["Neighborhood"]
+
+    #     preprocessor = ColumnTransformer(
+    #         transformers=[
+    #             ('num', StandardScaler(), numeric_features),
+    #             ('cat', OneHotEncoder(), categorical_features)
+    #         ]
+    #     )
+
+    #     X_train = preprocessor.fit_transform(X_train)
+    #     X_test = preprocessor.transform(X_test)
+    #     return X_test,X_train, preprocessor
+
+    def standardized_data(self, X_train, X_test):
+
+        numeric_features = ["SquareFeet","Bedrooms", "Bathrooms"]
         categorical_features = ["Neighborhood"]
 
         preprocessor = ColumnTransformer(
             transformers=[
                 ('num', StandardScaler(), numeric_features),
-                ('cat', OneHotEncoder(), categorical_features)
+                ('cat', OneHotEncoder(handle_unknown='ignore'), categorical_features)
             ]
         )
 
         X_train = preprocessor.fit_transform(X_train)
         X_test = preprocessor.transform(X_test)
-        return X_test,X_train
+
+        return X_train, X_test, preprocessor
 
     def train_model(self, X_train, y_train):
-        sgd_model = SGDRegressor(loss="squared_error", max_iter=1000, learning_rate="invscalling", eta=0.01)
+        sgd_model = SGDRegressor(loss="squared_error", max_iter=10000, learning_rate="invscaling", eta0=0.01)
         sgd_model.fit(X_train, y_train)
         return sgd_model
     
 if __name__ == "__main__":
     model = LinearRegressionModel()
-    data = model.load_data("C:\Users\92324\Desktop\ML_from_Sratch\ML_from_Scratch\Mlops\data\housing_price_dataset.csv")
-    data = model.drop_feature(data, "ID")
+    data = model.load_data("C:\\Users\\92324\\Desktop\\ML_from_Sratch\\ML_from_Scratch\\Mlops\\data\\housing_price_dataset.csv")
+    data = model.drop_feature(data, "YearBuilt")
     X, y = model.create_featur_label(data, "Price")
     X_train, X_test, y_train, y_test =model.train_test_split(X,y)
-    X_train = model.standarized_data(X_train, X_test)
+    X_train, X_test, preprocessor = model.standardized_data(X_train, X_test)
     sgd_model = model.train_model(X_train, y_train)
 
-    joblib.dump(sgd_model, "linear_regression_model.pkl")
+    joblib.dump({'model': sgd_model, 'preprocessor': preprocessor}, "linear_regression_model.pkl")
