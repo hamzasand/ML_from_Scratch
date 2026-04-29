d0a4d9604a3fe808bd0857af7a85266ebc434008
M.Hamza <152792061+hamzasand@users.noreply.github.com>
2026-04-29 18:44:33 +0500

    Update regression.py

--- Mlops/models_code/regression.py
+++ Mlops/models_code/regression.py
@@ -5,6 +5,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 
 from sklearn.preprocessing import StandardScaler, OneHotEncoder
+
 from sklearn.compose import ColumnTransformer
 from sklearn.linear_model import SGDRegressor
 import joblib
